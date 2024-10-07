618dc77b5bb4da84c63d6d1f83286abe73b1a3c4
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2024-10-07 17:58:38 +0200

    fix(admin): WB-3246, check angular locale validity before formatting a localized date

--- projects/ngx-ode-ui/src/lib/pipes/localizedDate.pipe.ts
+++ projects/ngx-ode-ui/src/lib/pipes/localizedDate.pipe.ts
@@ -1,4 +1,4 @@
-import {DatePipe} from '@angular/common';
+import {DatePipe, getLocaleId} from '@angular/common';
 import {Pipe, PipeTransform} from '@angular/core';
 import {BundlesService} from 'ngx-ode-sijil';
 
@@ -11,9 +11,20 @@ export class LocalizedDatePipe implements PipeTransform {
   constructor(private bundlesService: BundlesService) {
   }
 
-  transform(value: any, pattern: string = 'mediumDate'): any {
-    const datePipe: DatePipe = new DatePipe(this.bundlesService.currentLanguage);
+  transform(value: any, pattern: string = "mediumDate"): any {
+    let targetLanguage = this.bundlesService.currentLanguage || "en";
+    try {
+      // If targetLanguage and angular current locale are different, fall back angular locale to "en"
+      if (getLocaleId(targetLanguage) !== targetLanguage) {
+        targetLanguage = "en";
+      }
+    } catch (e) {
+      // getLocaleId(targetLanguage) can throw an error when locale is not recognized
+      // In such a case, fall back to "en"
+      targetLanguage = "en";
+    }
+    // Use angular locale formatting for localized dates
+    const datePipe: DatePipe = new DatePipe(targetLanguage);
     return datePipe.transform(value, pattern);
   }
-
 }
